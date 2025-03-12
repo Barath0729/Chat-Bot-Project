7bd48ba352eed02b26d3c256afa6609ac46ec344
Barath0729 <126565359+Barath0729@users.noreply.github.com>
2025-03-12 11:12:12 +0530

    Update search.py

--- search.py
+++ search.py
@@ -7,14 +7,3 @@ def search_query(query, vector_store):
     query_embedding = model.encode(query)
     results = vector_store.search(query_embedding, top_k=config.TOP_K)
     return " ".join(results)
-
-
-
-# #for chromadb
-# from embedding import generate_embeddings
-# from vector_store import VectorStore
-
-# def search_query(query, vector_store, top_k=5):
-#     query_embedding = generate_embeddings([query])[0]  # Generate query embedding
-#     results = vector_store.search(query_embedding, top_k)
-#     return " ".join(results)  # Combine retrieved texts
